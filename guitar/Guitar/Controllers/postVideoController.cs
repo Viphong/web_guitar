1ff3440084bb866e3727eed616f0ead18560116e
Viphong <13520537@gm.uit.edu.vn>
2016-11-13 13:30:06 +0700

    update3

---
+++ guitar/Guitar/Controllers/postVideoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Guitar.Controllers
+{
+    public class postVideoController : Controller
+    {
+        //
+        // GET: /postVideo/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
