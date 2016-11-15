9b38edf1361033ac721073e3c866414844cff5fd
Viphong <13520537@gm.uit.edu.vn>
2016-11-15 16:34:41 +0700

    update3

---
+++ guitar/Guitar/Controllers/LessionController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Guitar.Controllers
+{
+    public class LessionController : Controller
+    {
+        //
+        // GET: /Blog/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ guitar/Guitar/Controllers/LessonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Guitar.Controllers
+{
+    public class LessonController : Controller
+    {
+        //
+        // GET: /Lesson/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
